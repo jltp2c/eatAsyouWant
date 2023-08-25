e8008557f6b5245b7ba615c17f81417c501e0435
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-08-25 12:43:13 +0200

    calculate product price quantity done

--- src/app/menu/[category]/[id]/page.tsx
+++ src/app/menu/[category]/[id]/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { pizzas, pastas, burgers } from "../../../../../data";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import { title } from "process";
 
 interface pageProps {
   params: {
@@ -11,6 +12,12 @@ interface pageProps {
     id: number;
   };
 }
+
+type ProductProps = {
+  price: number;
+  options?: { title: string; additionalPrice: number }[];
+  additionalPrice: number;
+};
 
 function findProductById(id: number) {
   // Recherchez le produit dans les différents tableaux
@@ -20,6 +27,26 @@ function findProductById(id: number) {
 
 function SingleProduct({ params }: pageProps) {
   const product = findProductById(parseInt(params.id));
+
+  const [total, setTotal] = useState(product?.price);
+  const [quantity, setQuantity] = useState(1);
+  const [isSelected, setIsSelected] = useState(0);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (product) {
+      const selectedOption = product.options
+        ? product.options[isSelected]
+        : undefined;
+
+      const optionPrice = selectedOption ? selectedOption.additionalPrice : 0;
+
+      setTotal(quantity * (product.price + optionPrice || product.price));
+    }
+  }, [product, isSelected, quantity]);
 
   return (
     <div className="flex flex-col justify-center items-center h-[calc(100vh-10.9rem)] p-5 md:flex-row">
@@ -35,12 +62,17 @@ function SingleProduct({ params }: pageProps) {
       <div className="w-5/6 md:w-2/6">
         <h1 className="mb-5 text-red-500 font-bold">{product?.title}</h1>
         <p className="mb-5 text-red-500">{product?.desc}</p>
-        <p className="mb-5 text-red-500 font-bold">{product?.price} €</p>
+        <p className="mb-5 text-red-500 font-bold">{total?.toFixed(2)} €</p>
         <div className="mb-10 flex justify-center gap-12 ">
-          {product?.options?.map((option) => (
+          {product?.options?.map((option, index) => (
             <button
               className="bg-white p-2 text-black border-2 border-red-500 rounded-xl "
-              key={option.title}>
+              key={option.title}
+              style={{
+                background: isSelected === index ? "red" : "white",
+                color: isSelected === index ? "white" : "red",
+              }}
+              onClick={() => setIsSelected(index)}>
               {option.title}
             </button>
           ))}
@@ -49,9 +81,12 @@ function SingleProduct({ params }: pageProps) {
           <input
             type="number"
             placeholder="Quantity"
-            className="border-2 border-red-500 placeholder:pl-2 w-56 md:w-96"
-            max={9}
-            min={1}
+            className="border-2 border-red-500 placeholder:pl-2 w-56 md:w-96 p-2"
+            max="9"
+            min="1"
+            defaultValue="1"
+            onChange={(e) => setQuantity(parseInt(e.target.value))}
+            onKeyDown={handleKeyDown}
           />
           <button className="bg-red-500 p-2 text-white rounded-sm">
             Add in cart
